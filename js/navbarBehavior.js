e5d73ba85633d5036505cf8452e14e8abbe62d86
Naukri7707 <naukri7707@gmail.com>
2019-07-12 01:12:46 +0800

    Site updated: 2019-07-12 01:12:43

--- js/navbarBehavior.js
+++ js/navbarBehavior.js
@@ -1,28 +1,56 @@
 $(document).ready(function () {
-    const HIDDEN = '-50px';
-    const $navbar = $('.navbar');
-    const $window = $(window);
-    $window.scroll(function () {
-        if ($window.scrollTop() <= 360)
-        {
-            if ($window.scrollTop() == 0)
-                $navbar.css('position', 'relative');
-            else
-                $navbar.css('position', 'fixed');
-            $navbar.css('top', -$window.scrollTop() >> 3);
-        }
-        else
-            $navbar.css('top', HIDDEN);
-    });
 
-    $navbar.hover(
-        function () {
-            if ($navbar.css('top') < '0px')
-                $navbar.animate({ top: '0px' }, 500, 'swing');
-        },
-        function () {
-            if ($navbar.css('top') > HIDDEN)
-                $navbar.delay("fast").animate({ top: $window.scrollTop() <= 360 ? -$window.scrollTop() >> 3 : HIDDEN }, 500, 'swing');
-        }
-    );
+  const HIDDEN = 45;
+  const POW = 3;
+  const $navbar = $('.navbar');
+  const $window = $(window);
+
+  if ($window.scrollTop() <= (HIDDEN << POW))
+  {
+    $navbar.css('position', $window.scrollTop() === 0 ? 'relative' : 'fixed')
+    $navbar.css('top', -($window.scrollTop() >> POW) + 'px');
+  }
+  else
+  {
+    $navbar.css('position', 'fixed').css('top', -HIDDEN + 'px');
+  }
+
+  $window.scroll(function () {
+    if ($window.scrollTop() <= (HIDDEN << POW))
+    {
+      $navbar.css('position', $window.scrollTop() === 0 ? 'relative' : 'fixed')
+      $navbar.css('top', -($window.scrollTop() >> POW) + 'px');
+    }
+    else
+    {
+      $navbar.css('position', 'fixed').css('top', -HIDDEN + 'px');
+    }
+  });
+
+  $navbar.hover(
+    function () {
+      if ($navbar.css('top') < '0px')
+      {
+        $navbar.animate({ top: '0px' }, 500, 'swing');
+      }
+    },
+    function () {
+      if ($navbar.css('top') > -HIDDEN + 'px')
+      {
+        $navbar.delay('fast').animate({ top: $window.scrollTop() <= HIDDEN << 3 ? -$window.scrollTop() >> 3 : -HIDDEN + 'px' }, 500, 'swing');
+      }
+    }
+  );
 });
+
+function scrolling($navbar, $window, HIDDEN) {
+  if ($window.scrollTop() <= (HIDDEN << 4))
+  {
+    $navbar.css('position', $window.scrollTop() === 0 ? 'relative' : 'fixed')
+    $navbar.css('top', -($window.scrollTop() >> 4) + 'px');
+  }
+  else
+  {
+    $navbar.css('position', 'fixed').css('top', -HIDDEN + 'px');
+  }
+}
