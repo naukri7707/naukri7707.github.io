712af451a76c972cb777682058866e2aa3019c35
Naukri7707 <naukri7707@gmail.com>
2019-07-15 18:57:25 +0800

    Site updated: 2019-07-15 18:57:24

--- js/indexBehavior.js
+++ js/indexBehavior.js
@@ -11,17 +11,39 @@ $titles.each(function () {
   });
 })
 
-
-
 // 閱讀更多效果
 $('.post').each(function () {
-  if ($(this).height() < 700)
+  if ($(this).css('height') < $(this).css('max-height'))
   {
-    $(this).children(".more").remove();
+    $(this).children('.more').remove();
   }
 });
 
 function readMore() {
   $(event.target).parents('.post').css('max-height', '99999px');
   $(event.target).parent().remove();
+}
+
+// 程式碼複製
+$('.highlight tr').each(function () {
+
+  $(this).append('<td class="copy-button" onclick="copyCode()">複製</td>');
+});
+
+function copyCode() {
+  var code;
+  $(event.target).parent().find('.code .line').each(function () {
+    code += $(this)[0].innerText + "\n";
+  });
+  Copy(code);
+}
+
+function Copy(text) {
+  var tmp = document.createElement('textarea');
+  tmp.value = text;
+  document.body.appendChild(tmp);
+  tmp.select();
+  document.execCommand("Copy");
+  alert('複製成功');
+  tmp.remove();
 }
