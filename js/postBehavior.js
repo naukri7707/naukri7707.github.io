8c778c4ba63d47821511df0d0ff52c99f0768819
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-01 14:54:50 +0800

    Site updated: 2019-08-01 14:54:49

--- js/postBehavior.js
+++ js/postBehavior.js
@@ -1,31 +1,40 @@
-
-$(function () {
+$(function() {
   // 動態嵌入程式碼複製按鈕
-  $('.highlight').find('.code').append('<td class="copy-button" onclick="copyCode()">Copy</td>');
+  $(".highlight")
+    .find(".code")
+    .append('<td class="copy-button" onclick="copyCode()">Copy</td>');
 });
 
 // 閱讀更多效果
 function readMore() {
-  $(event.target).parents('.post-inner').find('.hidden-content').css('display', 'block');
-  $(event.target).parent().remove();
+  $(event.target)
+    .parents(".post-inner")
+    .find(".hidden-content")
+    .css("display", "block");
+  $(event.target)
+    .parent()
+    .remove();
 }
 
 // 程式碼複製效果
 function copyCode() {
   var code = "";
-  $(event.target).parent().find('.line').each(function () {
-    code += $(this)[0].innerText + "\n";
-  });
+  $(event.target)
+    .parent()
+    .find(".line")
+    .each(function() {
+      code += $(this)[0].innerText + "\n";
+    });
   Copy(code);
 }
 
 // 複製到剪貼簿
 function Copy(text) {
-  var tmp = document.createElement('textarea');
+  var tmp = document.createElement("textarea");
   tmp.value = text;
   document.body.appendChild(tmp);
   tmp.select();
   document.execCommand("Copy");
   tmp.remove();
-  showMessage('複製成功');
+  showMessage("複製成功");
 }
