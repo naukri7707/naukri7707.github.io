f3d0747877c1b1fbd45aafd9100ab5cc4eb4fe4d
Naukri <50033668+naukri7707@users.noreply.github.com>
2019-08-11 19:24:38 +0800

    Site updated: 2019-08-11 19:24:33

--- js/layoutBehavior.js
+++ js/layoutBehavior.js
@@ -1,15 +1,34 @@
-var pervScrollTop = $(window).scrollTop();
-
 $(function () {
+  // 擴充給 addclass() 一個回調函數
+  const oAddClass = $.fn.addClass;
+  $.fn.addClass = function () {
+    for (var i in arguments) {
+      var arg = arguments[i];
+      if (!!(arg && arg.constructor && arg.call && arg.apply)) {
+        setTimeout(arg.bind(this));
+        delete arguments[i];
+      }
+    }
+    return oAddClass.apply(this, arguments);
+  }
+
   const $navbar = $('#navbar');
+  if ($(window).scrollTop() > 0) {
+    $navbar.removeClass('banner-style').addClass('normal-style', function () {
+      $navbar.css('transition', 'background-color 400ms');
+    });
+  }
+  else {
+    $navbar.css('transition', 'background-color 400ms');
+  }
+
   $(window).scroll(function () {
-    if ($(window).scrollTop() <= pervScrollTop) {
-      $navbar.css('transform', 'translateY(0)');
+    if ($(window).scrollTop() <= 20) {
+      $navbar.removeClass('normal-style').addClass('banner-style');
     }
     else {
-      $navbar.css('transform', 'translateY(-' + $navbar.css('height') + ')');
+      $navbar.removeClass('banner-style').addClass('normal-style');
     }
-    pervScrollTop = $(window).scrollTop();
   });
 });
 
